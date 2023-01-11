0f613eae38c9c78c6447d8800fe890a8eb55ae94
Andrew Blair <andrew.blair@ucsf.edu>
2023-01-11 15:52:01 -0800

    Cleanup example v0.1.0 RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,6 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../src/example'))
-# sys.path.insert(0, os.path.abspath('../../src/'))
 
 project = 'Minimal-Example'
 copyright = '2023, Andrew Blair'
@@ -27,15 +26,6 @@ master_doc = 'index'
 
 
 # -- General configuration
-
-# extensions = [
-#     'sphinx.ext.duration',
-#     'sphinx.ext.doctest',
-#     'sphinx.ext.autodoc',
-#     'sphinx.ext.autosummary',
-#     'sphinx.ext.intersphinx',
-# ]
-
 extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
 
 
